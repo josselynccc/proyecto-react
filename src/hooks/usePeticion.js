83ffe829c273141640a30c583e7984e7584fd685
Josselyn Cortez <cristheljosselyn1@gmail.com>
2025-06-19 18:46:43 -0500

    UPDATE usePeticion

--- src/hooks/usePeticion.js
+++ src/hooks/usePeticion.js
@@ -6,27 +6,31 @@ const usePeticion=(endpoint)=>{
     const [data,setdata]=useState([])
     const API_URL=import.meta.env.VITE_API_URL
 
-    useEffect(()=>{
-        axios.get(`${API_URL}${endpoint}`,{
-          headers: {
-            Authorization: 'Bearer 3ffe6bc2db168942c86803d2a75d07d66554b8afb7ec7837cdd0498d38255f85'
-          } 
-        })
-        .then(async (data) =>{
-            if (typeof endpoint === "string") {
-                data = await axios.get(`${API_URL}${endpoint}`);
-              } else if (Array.isArray(endpoint)) {
-                data = await axios.get(`${API_URL}${endpoint[0]}`);
-              } else if (typeof endpoint === "object") {
-                data = await axios.get(`${API_URL}${endpoint.path}`);
-              }
-            /* console.log(data.data.data) */
-            setdata(data.data.data)
-        })
-        .catch(()=>{
-            console.log('ALGO FALLO... ')
-        })
-    },[])
+    useEffect(() => {
+    let url = "";
+
+    if (typeof endpoint === "string") {
+      url = `${API_URL}${endpoint}`;
+    } else if (Array.isArray(endpoint)) {
+      url = `${API_URL}${endpoint[0]}`;
+    } else if (typeof endpoint === "object") {
+      url = `${API_URL}${endpoint.path}`;
+    }
+
+    axios
+      .get(url, {
+        headers: {
+          Authorization:
+            "Bearer 3ffe6bc2db168942c86803d2a75d07d66554b8afb7ec7837cdd0498d38255f85",
+        },
+      })
+      .then((response) => {
+        setdata(response.data.data);
+      })
+      .catch((err) => {
+        console.log("ALGO FALLÓ... ", err);
+      });
+  }, []);
 
     return data
 }
